a1f2bf782d24e3aa61d0dca1dc7cad6f95746885
Pedro Dutra <ppmdutra@gmail.com>
2022-07-13 09:13:08 -0300

    adding lang attributes and applying better indentation

--- script.js
+++ script.js
@@ -3,43 +3,42 @@
 function init() {
     // run whatever we need
    
-if (document.querySelector("#codewars")) {
-async function getInfo() {
-    let url = "https://www.codewars.com/api/v1/users/Dutz145";
+    if (document.querySelector("#codewars")) {
+        async function getInfo() {
+            let url = "https://www.codewars.com/api/v1/users/Dutz145";
 
-    return await fetch(url, {
-        method:"GET"
-    });
-}
+            return await fetch(url, {
+                method:"GET"
+            });
+        }
 
-const content = getInfo().then((response) =>{
-    if (response.ok === true) {return response.json()}
-    throw  new Error("response failed")
-}, netError => {
-    console.log(netError)
-})
-.then(user => {
-    let content = [`Username: ${user.username}`, `Rank: ${user.ranks.overall.name}`, `Completed Kata: ${user.codeChallenges.totalCompleted}`];
+        getInfo().then((response) =>{
+            if (response.ok === true) {return response.json()}
+            throw  new Error("response failed")
+        }, netError => {
+            console.log(netError)
+        })
+        .then(user => {
+            let content = [`Username: ${user.username}`, `Rank: ${user.ranks.overall.name}`, `Completed Kata: ${user.codeChallenges.totalCompleted}`];
 
-    const div = document.getElementById("codewars")
-    const ul = document.createElement("ul")
-    ul.setAttribute('class', 'nobullets')
+            const div = document.getElementById("codewars")
+            const ul = document.createElement("ul")
+            ul.setAttribute('class', 'nobullets')
 
-    for (let i in content) {
-        let li = document.createElement("li")
-        li.textContent = content[i]
-        ul.append(li)
+            for (let i in content) {
+                let li = document.createElement("li")
+                li.textContent = content[i]
+                ul.append(li)
+            }
+
+            let img = document.createElement('img')
+            img.setAttribute('src', 'https://www.codewars.com/users/Dutz145/badges/large?logo=false')
+            
+            div.append(ul)
+            div.append(img)
+
+        })
     }
-
-    div.append(ul)
-
-    let img = document.createElement('img')
-    img.setAttribute('src', 'https://www.codewars.com/users/Dutz145/badges/large?logo=false')
-
-    div.append(img)
-
-})
-}
 };
 
 const swup = new Swup();
