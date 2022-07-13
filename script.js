a414c09c7e8cfef3a3093db1dd282787faed0f5f
Pedro Dutra <ppmdutra@gmail.com>
2022-07-12 21:28:52 -0300

    adding transitions and modifying css

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+function init() {
+    // run whatever we need
+   
+
+async function getInfo() {
+    let url = "https://www.codewars.com/api/v1/users/Dutz145";
+
+    return await fetch(url, {
+        method:"GET"
+    });
+}
+
+const content = getInfo().then((response) =>{
+    if (response.ok === true) {return response.json()}
+    throw  new Error("response failed")
+}, netError => {
+    console.log(netError)
+})
+.then(user => {
+    let content = [`Username: ${user.username}`, `Rank: ${user.ranks.overall.name}`, `Completed Kata: ${user.codeChallenges.totalCompleted}`];
+
+    const div = document.getElementById("codewars")
+    const ul = document.createElement("ul")
+    ul.setAttribute('class', 'nobullets')
+
+    for (let i in content) {
+        let li = document.createElement("li")
+        li.textContent = content[i]
+        ul.append(li)
+    }
+
+    div.append(ul)
+
+    let img = document.createElement('img')
+    img.setAttribute('src', 'https://www.codewars.com/users/Dutz145/badges/large?logo=false')
+
+    div.append(img)
+
+})
+};
+
+
+const swup = new Swup();
+
+init()
+
+swup.on('contentReplaced', init);
